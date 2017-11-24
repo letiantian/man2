1ca6a5e04084bedd1fa854565e5e5870c8d20d3f
letian <sunlt1699@gmail.com>
2017-11-24 22:24:55 +0800

    update to v0.0.2

--- README.md
+++ README.md
@@ -4,11 +4,22 @@ This tool is designed to get the usage of commands quickly.
 
 这个工具用于更快地查找命令使用方法。
 
-## Install
+## Install man2
+
+```plain
+$ npm install man2 -g
+```
+
+## Install documents
 
 ```plain
 $ git clone https://github.com/letiantian/man2-docs.git ~/.man2-docs
-$ npm install man2 -g
+```
+
+If it is neccessary to update documents, run:
+```plain
+$ cd ~/.man2-docs
+$ git pull origin master
 ```
 
 ## Usage
--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function get_manual(file_path) {
     return new Promise(function(resolve, reject){
         fs.readFile(file_path, 'utf8', function (err,data) {
             if (err) {
+                console.log('can not find the command usage file(s) in: \n\n\t%s\n', file_path);
                 resolve(false);
             } else {
                 console.log(marked(data));
@@ -43,21 +44,10 @@ function get_manual(file_path) {
 }
 
 function show_manual(locale, cmd) {
-    var backup_locale = locale.split('_')[0];
-    var file_path_1 = joinPath(process.env.HOME, '.man2-docs', locale, cmd+'.md');
-    var file_path_2;
-    if (backup_locale && locale!=backup_locale) {
-        file_path_2 = joinPath(process.env.HOME, '.man2-docs', backup_locale, cmd+'.md');
-    }
+    var file_path = joinPath(process.env.HOME, '.man2-docs', locale, cmd+'.md');
 
-    get_manual(file_path_1).then(function(result){
-        if (!result && !!file_path_2) {
-            get_manual(file_path_2).then(function(result){
-                if (!result) {
-                    console.log('can not find the command usage file(s) in: \n\n\t%s\n\t%s\n', file_path_1, file_path_2);
-                }
-            });
-        }
+    get_manual(file_path).then(function(result){
+        // ...
     });
 
 }
@@ -86,7 +76,6 @@ if (process.argv.length == 2) {
     show_help();
 } else {
     var cmd = process.argv.slice(2).join(' ');
-    // console.log(cmd)
     get_locale().then(function(locale){
         show_manual(locale, cmd);
     });
